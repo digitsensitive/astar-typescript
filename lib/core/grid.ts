5251c6874453bdacecbf2c20f4ff3807dab7597b
digitsensitive <digit.sensitivee@gmail.com>
2020-04-14 21:23:27 +0200

    lower case file names

---
+++ lib/core/grid.ts
@@ -0,0 +1,218 @@
+/**
+ * @description Grid class
+ * @author Digitsensitive <digit.sensitivee@gmail.com>
+ * @copyright 2017 - 2020 Digitsensitive
+ * @license {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+import { Node } from './node';
+import { IGridConstructor, IPoint } from '../interfaces/astar.interfaces';
+
+export class Grid {
+  // General properties
+  readonly width: number;
+  readonly height: number;
+  readonly numberOfFields: number;
+
+  // The node grid
+  private gridNodes: Node[][];
+
+  constructor(aParams: IGridConstructor) {
+    // Set the general properties
+    if (aParams.width && aParams.height) {
+      this.width = aParams.width;
+      this.height = aParams.height;
+      this.numberOfFields = this.width * this.height;
+    } else if (aParams.matrix) {
+      this.width = aParams.matrix[0].length;
+      this.height = aParams.matrix.length;
+      this.numberOfFields = this.width * this.height;
+    }
+
+    // Create and generate the matrix
+    this.gridNodes = this.buildGridWithNodes(
+      aParams.matrix || undefined,
+      this.width,
+      this.height,
+      aParams.densityOfObstacles || 0
+    );
+  }
+
+  /**
+   * Build grid, fill it with nodes and return it.
+   * @param matrix [ 0 or 1: 0 = walkable; 1 = not walkable ]
+   * @param width [grid width]
+   * @param height [grid height]
+   * @param densityOfObstacles [density of non walkable fields]
+   */
+  private buildGridWithNodes(
+    matrix: number[][],
+    width: number,
+    height: number,
+    densityOfObstacles?: number
+  ): Node[][] {
+    let newGrid: Node[][] = [];
+    let id: number = 0;
+
+    // Generate an empty matrix
+    for (let y = 0; y < height; y++) {
+      newGrid[y] = [];
+      for (let x = 0; x < width; x++) {
+        newGrid[y][x] = new Node({
+          id: id,
+          xPos: x,
+          yPos: y
+        });
+
+        id++;
+      }
+    }
+
+    /**
+     * If we have not loaded a predefined matrix,
+     * loop through our grid and set random obstacles.
+     */
+    if (matrix === undefined) {
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          let rndNumber = Math.floor(Math.random() * 10) + 1;
+          if (rndNumber > 10 - densityOfObstacles) {
+            newGrid[y][x].setIsWalkable(false);
+          } else {
+            newGrid[y][x].setIsWalkable(true);
+          }
+        }
+      }
+
+      return newGrid;
+    }
+
+    /**
+     * In case we have a matrix loaded.
+     * Load up the informations of the matrix.
+     */
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        if (matrix[y][x]) {
+          newGrid[y][x].setIsWalkable(false);
+        } else {
+          newGrid[y][x].setIsWalkable(true);
+        }
+      }
+    }
+
+    return newGrid;
+  }
+
+  /**
+   * Return a specific node.
+   * @param position [position on the grid]
+   */
+  public getNodeAt(position: IPoint): Node {
+    return this.gridNodes[position.y][position.x];
+  }
+
+  /**
+   * Check if specific node walkable.
+   * @param position [position on the grid]
+   */
+  public isWalkableAt(position: IPoint): boolean {
+    return this.gridNodes[position.y][position.x].getIsWalkable();
+  }
+
+  /**
+   * Check if specific node is on the grid.
+   * @param position [position on the grid]
+   */
+  private isOnTheGrid(position: IPoint): boolean {
+    return (
+      position.x >= 0 &&
+      position.x < this.width &&
+      position.y >= 0 &&
+      position.y < this.height
+    );
+  }
+
+  /**
+   * Get surrounding nodes.
+   * @param currentXPos [x-position on the grid]
+   * @param currentYPos [y-position on the grid]
+   * @param diagnonalMovementAllowed [is diagnonal movement allowed?]
+   */
+  public getSurroundingNodes(
+    currentPosition: IPoint,
+    diagnonalMovementAllowed: boolean
+  ): Node[] {
+    let surroundingNodes: Node[] = [];
+
+    for (var y = currentPosition.y - 1; y <= currentPosition.y + 1; y++) {
+      for (var x = currentPosition.x - 1; x <= currentPosition.x + 1; x++) {
+        if (this.isOnTheGrid({ x, y })) {
+          if (this.isWalkableAt({ x, y })) {
+            if (diagnonalMovementAllowed) {
+              surroundingNodes.push(this.getNodeAt({ x, y }));
+            } else {
+              if (x == currentPosition.x || y == currentPosition.y) {
+                surroundingNodes.push(this.getNodeAt({ x, y }));
+              }
+            }
+          } else {
+            continue;
+          }
+        } else {
+          continue;
+        }
+      }
+    }
+
+    return surroundingNodes;
+  }
+
+  public setGrid(newGrid: Node[][]): void {
+    this.gridNodes = newGrid;
+  }
+
+  /**
+   * Reset the grid
+   */
+  public resetGrid(): void {
+    for (let y = 0; y < this.gridNodes.length; y++) {
+      for (let x = 0; x < this.gridNodes[y].length; x++) {
+        this.gridNodes[y][x].setIsOnClosedList(false);
+        this.gridNodes[y][x].setIsOnOpenList(false);
+        this.gridNodes[y][x].setParent(undefined);
+        this.gridNodes[y][x].setFGHValuesToZero();
+      }
+    }
+  }
+
+  /**
+   * Get all the nodes of the grid.
+   */
+  public getGridNodes(): Node[][] {
+    return this.gridNodes;
+  }
+
+  /**
+   * Get a clone of the grid
+   */
+  public clone(): Node[][] {
+    let cloneGrid: Node[][] = [];
+    let id: number = 0;
+
+    for (let y = 0; y < this.height; y++) {
+      cloneGrid[y] = [];
+      for (let x = 0; x < this.width; x++) {
+        cloneGrid[y][x] = new Node({
+          id: id,
+          xPos: x,
+          yPos: y,
+          walkable: this.gridNodes[y][x].getIsWalkable()
+        });
+
+        id++;
+      }
+    }
+    return cloneGrid;
+  }
+}
